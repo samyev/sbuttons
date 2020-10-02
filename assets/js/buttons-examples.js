5a7133f7ae2899ce59a52dbe90dad520f87c5d9f
samyev <sam1967evelyn@gmail.com>
2020-10-02 10:45:20 -0300

    removed the isIOS function

--- assets/js/buttons-examples.js
+++ assets/js/buttons-examples.js
@@ -426,26 +426,3 @@ $(document).ready(function () {
     section.appendTo(content).before('<hr class="secondary-hr">');
   }
 });
-
-    function isIOS() {    
-        var ua = navigator.userAgent.toLowerCase();
-
-        var uMobile = '';
-
-        //Lista de dispositivos que acessar
-        uMobile = '';
-        uMobile += 'iphone;ipod;android';
-
-        //Separa os itens em arrays
-        v_uMobile = uMobile.split(';');
-
-        //verifica se vocÃª estÃ¡ acessando pelo celular
-        var isApple = false;
-        for (i=0;i<=v_uMobile.length;i++){
-            if (ua.indexOf(v_uMobile[i]) != -1){
-    	       isApple = true;
-            }
-        }
-
-        alert('is apple:' + isApple)
-
